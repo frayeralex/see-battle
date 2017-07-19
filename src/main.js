c5deb29dc169abfa0f5548c67bde8da86526a737
frayeralex <frayeralex@gmail.com>
2017-07-19 20:55:37 +0300

    set dev env

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
-/**
- * Created by alex on 08.07.17.
- */
+import './main.styl';
+import Store from './store/Store';
+
+// Components
+import Statistic from './components/statistic/Statistic';
+import Grid from './components/grid/Grid';
+
+//
+import GridComposer from './components/grid/GridComposer';
+
+const store = Store.init({
+    cells: [],
+    ships: [],
+});
+
+const userGridNode = document.querySelector('.sea-blocks .user');
+const userGrid = Grid.init(userGridNode, store);
+
+GridComposer.init(userGrid, store);
+
+new Statistic(document.querySelector('#statistic'), store);
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,60 @@
-/**
- * Created by alex on 08.07.17.
- */
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        app: './src/main.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.[hash:8].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.styl$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: 'css-loader',
+
+                    },
+                    {
+                        loader: 'stylus-loader',
+                    }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                    }
+                }
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3000,
+        stats: 'minimal',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'sea-battle',
+            template: './src/index.html',
+            mobile: true,
+            appMountId: 'app',
+        })
+    ]
+};
